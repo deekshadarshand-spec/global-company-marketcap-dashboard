1829a000d0ca4291861e3294eb6bca44bb38ab4a
deekshadarshand-spec <deekshadarshand@gmail.com>
2026-06-04 02:54:43 -0400

    Create data_loader.py

---
+++ utils/data_loader.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def load_data():
+
+    df = pd.read_csv("data/company_data.csv")
+
+    df["market_cap"] = (
+        df["market cap(Billion USD)"]
+        .astype(str)
+        .str.replace(",", "")
+        .astype(float)
+    )
+
+    df["world_rank"] = pd.to_numeric(
+        df["world_rank"],
+        errors="coerce"
+    )
+
+    return df
